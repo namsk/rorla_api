5e9ba191dbe5f87181969df05c727356ee984092
SeungKyun Nam <skyun.nam@gmail.com>
2014-02-21 14:15:43 +0900

    Comment 모델 작성
    * add migration for comment model
    * add relations into post and comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140221022955) do
+ActiveRecord::Schema.define(version: 20140221050653) do
 
   create_table "auth_tokens", force: true do |t|
     t.integer  "user_id"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20140221022955) do
   create_table "comments", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "content"
+    t.integer  "post_id"
   end
 
   create_table "posts", force: true do |t|
